94a55e9687599bc6822a2a467b82c2cbbce41610
Suneet Misra <hello@suneet.codes>
2025-01-27 11:25:55 +0000

    🎨 Adds nextJS support for mdx and frontmatter

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import { withContentCollections } from "@content-collections/next";
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+  },
+});
+
 // withContentCollections must be the outermost plugin
-export default withContentCollections(nextConfig);
+export default withContentCollections(withMDX(nextConfig));
